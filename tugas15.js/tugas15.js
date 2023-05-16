0a1a57dccdd79858ec2e1549826edcf27abdf895
tri ariyanto <triyanto129@gmail.com>
2023-05-16 23:48:09 +0700

    tugas 15 js
    mengumpulkan tugas

---
+++ tugas15.js/tugas15.js
@@ -0,0 +1,9 @@
+function sisimiring(){
+    var ab = 8 ;
+    console.log("nilai AB :",ab);
+    var bc = 6 ;
+    console.log("Nilai BC :", bc);
+    var AC = Math.pow(ab,2) + Math.pow(bc,2)
+    console.log("panjang sisi AC pada segitiga siku-siku tersebut adalah :",Math.sqrt(AC),"cm");
+}
+sisimiring();
